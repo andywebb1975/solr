029decccecca24d2475541ad0b54cd0d94976a72
Andy Webb <andywebb1975@gmail.com>
2023-01-27 20:00:18 +0000

    Add options to show and sort by maxScore for spelling collations

--- solr/core/src/java/org/apache/solr/spelling/SpellCheckCollator.java
+++ solr/core/src/java/org/apache/solr/spelling/SpellCheckCollator.java
@@ -49,6 +49,8 @@ public class SpellCheckCollator {
   private boolean suggestionsMayOverlap = false;
   private int docCollectionLimit = 0;
   private boolean getAllCollations = false;
+  private boolean getMaxScore = false;
+  private String sortMaxScore;
 
   public List<SpellCheckCollation> collate(
       SpellingResult result, String originalQuery, ResponseBuilder ultimateResponse) {
@@ -103,6 +105,7 @@ public class SpellCheckCollator {
       PossibilityIterator.RankedSpellPossibility possibility = possibilityIter.next();
       String collationQueryStr = getCollation(originalQuery, possibility.corrections);
       long hits = 0;
+      float maxScore = 0;
 
       if (verifyCandidateWithQuery) {
         tryNo++;
@@ -128,13 +131,22 @@ public class SpellCheckCollator {
         if (docCollectionLimit > 0) {
           params.set(CommonParams.MAX_HITS_ALLOWED, docCollectionLimit);
         }
-        // we don't want any stored fields
-        params.set(CommonParams.FL, ID);
-        // we'll sort by doc id to ensure no scoring is done.
-        params.set(CommonParams.SORT, "_docid_ asc");
+        if (getMaxScore) {
+          // we don't need any stored fields, but score is needed for maxScore to show up
+          params.set(CommonParams.FL, ID + ",score");
+          params.set(CommonParams.SORT, "score desc");
+          // TODO write tests for this path
+        } else {
+          // we don't want any stored fields
+          params.set(CommonParams.FL, ID);
+          // we'll sort by doc id to ensure no scoring is done.
+          params.set(CommonParams.SORT, "_docid_ asc");
+        }
         // CursorMark does not like _docid_ sorting, and we don't need it.
         params.remove(CursorMarkParams.CURSOR_MARK_PARAM);
         // If a dismax query, don't add unnecessary clauses for scoring
+        // NB edismax boost isn't removed - presumably this code predates that.
+        // This means it can still be used to influence the maxScore, which is helpful.
         params.remove(DisMaxParams.TIE);
         params.remove(DisMaxParams.PF);
         params.remove(DisMaxParams.PF2);
@@ -171,6 +183,7 @@ public class SpellCheckCollator {
                               SolrQueryResponse.RESPONSE_HEADER_APPROXIMATE_TOTAL_HITS_KEY,
                               checkResponse.rsp.getToLog().get("hits")))
                   .longValue();
+          if (getMaxScore) maxScore = checkResponse.getResults().docList.maxScore();
         } catch (Exception e) {
           log.warn(
               "Exception trying to re-query to check if a spell check possibility would return any hits.",
@@ -184,6 +197,10 @@ public class SpellCheckCollator {
         SpellCheckCollation collation = new SpellCheckCollation();
         collation.setCollationQuery(collationQueryStr);
         collation.setHits(hits);
+        if (getMaxScore) {
+          collation.setMaxScore(maxScore);
+          collation.setSortMaxScore(sortMaxScore);
+        }
         collation.setInternalRank(
             suggestionsMayOverlap
                 ? ((possibility.rank * 1000) + possibility.index)
@@ -291,4 +308,13 @@ public class SpellCheckCollator {
     return this;
   }
 
+  public SpellCheckCollator setGetMaxScore(boolean getMaxScore) {
+    this.getMaxScore = getMaxScore;
+    return this;
+  }
+
+  public SpellCheckCollator setSortMaxScore(String sortMaxScore) {
+    this.sortMaxScore = sortMaxScore;
+    return this;
+  }
 }
