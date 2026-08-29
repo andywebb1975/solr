61255fc3a80ba26bcf6ff742933e29e668a379b8
Andy Webb <andywebb@apache.org>
2026-08-29 14:59:52 +0100

    redundant logging

--- solr/core/src/java/org/apache/solr/spelling/DirectSolrSpellChecker.java
+++ solr/core/src/java/org/apache/solr/spelling/DirectSolrSpellChecker.java
@@ -230,8 +230,6 @@ public class DirectSolrSpellChecker extends SolrSpellChecker {
         result.add(token, empty);
       } else {
         for (SuggestWord suggestion : suggestions) {
-          // there's nowhere to record the score :-(
-          log.debug("token: {}, suggestion: {}", token.toString(), suggestion.toString());
           result.add(token, suggestion);
         }
       }
